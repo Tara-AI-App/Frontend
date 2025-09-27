0f159b0464d643ab9aa01e8370f54c86e74b3dca
gemm123 <gemaakbar213@gmail.com>
2025-09-27 15:59:02 +0700

    fix: dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
@@ -6,6 +8,13 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(process.cwd(), '.'),
+    };
+    return config;
   },
 };
 
